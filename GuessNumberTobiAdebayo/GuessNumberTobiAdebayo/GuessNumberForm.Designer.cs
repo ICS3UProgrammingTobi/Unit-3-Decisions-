1713f5a7bf6a37ab0ba4b19143d9feb47f3da3fd
s487316 <tobi.adebayo@ocsbstudent.ca>
2019-10-17 13:09:44 -0400

    push

--- GuessNumberTobiAdebayo/GuessNumberTobiAdebayo/GuessNumberForm.Designer.cs
+++ GuessNumberTobiAdebayo/GuessNumberTobiAdebayo/GuessNumberForm.Designer.cs
@@ -28,20 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblInteger = new System.Windows.Forms.Label();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.lblRight = new System.Windows.Forms.Label();
+            this.lblWrong = new System.Windows.Forms.Label();
+            this.picX = new System.Windows.Forms.PictureBox();
+            this.picCheck = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picX)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCheck)).BeginInit();
             this.SuspendLayout();
+            // 
+            // lblInteger
+            // 
+            this.lblInteger.AutoSize = true;
+            this.lblInteger.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInteger.Location = new System.Drawing.Point(12, 85);
+            this.lblInteger.Name = "lblInteger";
+            this.lblInteger.Size = new System.Drawing.Size(386, 25);
+            this.lblInteger.TabIndex = 0;
+            this.lblInteger.Text = "Guess an integer between 1 and 10";
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(36, 170);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(75, 23);
+            this.btnCheck.TabIndex = 1;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.BtnCheck_Click);
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Location = new System.Drawing.Point(465, 85);
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.Size = new System.Drawing.Size(100, 20);
+            this.txtAnswer.TabIndex = 2;
+            // 
+            // lblRight
+            // 
+            this.lblRight.AutoSize = true;
+            this.lblRight.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRight.Location = new System.Drawing.Point(244, 166);
+            this.lblRight.Name = "lblRight";
+            this.lblRight.Size = new System.Drawing.Size(255, 25);
+            this.lblRight.TabIndex = 3;
+            this.lblRight.Text = "You guessed correctly!";
+            this.lblRight.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // lblWrong
+            // 
+            this.lblWrong.AutoSize = true;
+            this.lblWrong.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWrong.Location = new System.Drawing.Point(244, 232);
+            this.lblWrong.Name = "lblWrong";
+            this.lblWrong.Size = new System.Drawing.Size(220, 25);
+            this.lblWrong.TabIndex = 4;
+            this.lblWrong.Text = "You guessed wrong";
+            // 
+            // picX
+            // 
+            this.picX.Image = global::GuessNumberTobiAdebayo.Properties.Resources.red_x;
+            this.picX.Location = new System.Drawing.Point(529, 207);
+            this.picX.Name = "picX";
+            this.picX.Size = new System.Drawing.Size(100, 50);
+            this.picX.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picX.TabIndex = 6;
+            this.picX.TabStop = false;
+            // 
+            // picCheck
+            // 
+            this.picCheck.Image = global::GuessNumberTobiAdebayo.Properties.Resources.checkmark;
+            this.picCheck.Location = new System.Drawing.Point(529, 141);
+            this.picCheck.Name = "picCheck";
+            this.picCheck.Size = new System.Drawing.Size(100, 50);
+            this.picCheck.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picCheck.TabIndex = 5;
+            this.picCheck.TabStop = false;
             // 
             // frmGuessNumber
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(641, 364);
+            this.Controls.Add(this.picX);
+            this.Controls.Add(this.picCheck);
+            this.Controls.Add(this.lblWrong);
+            this.Controls.Add(this.lblRight);
+            this.Controls.Add(this.txtAnswer);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.lblInteger);
             this.Name = "frmGuessNumber";
             this.Text = "Guess number by Tobi";
+            ((System.ComponentModel.ISupportInitialize)(this.picX)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCheck)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblInteger;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.TextBox txtAnswer;
+        private System.Windows.Forms.Label lblRight;
+        private System.Windows.Forms.Label lblWrong;
+        private System.Windows.Forms.PictureBox picCheck;
+        private System.Windows.Forms.PictureBox picX;
     }
 }
 
--- GuessNumberTobiAdebayo/GuessNumberTobiAdebayo/GuessNumberForm.cs
+++ GuessNumberTobiAdebayo/GuessNumberTobiAdebayo/GuessNumberForm.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+     * Created by: Tobi Adebayo
+     * Created on: 17-10-2019
+     * Created for: ICS3U Programming
+     * Daily Assignment – Day #16 - Guess The Number
+     * This program allows the user to guess a random number the computer generated
+    */
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +22,52 @@ namespace GuessNumberTobiAdebayo
         public frmGuessNumber()
         {
             InitializeComponent();
+            //Hide the label
+            this.lblRight.Hide();
+            this.lblWrong.Hide();
+            this.picCheck.Hide();
+            this.picX.Hide();
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnCheck_Click(object sender, EventArgs e)
+        {
+            //Declare constants and variables
+            const int SCREAT_NUMBER = 5;
+            int number;
+
+            //get the users number
+            number = int.Parse(txtAnswer.Text);
+
+            //If the number guessed is correct or wrong
+            if(number== SCREAT_NUMBER) 
+            {
+                lblRight.Show();
+                picCheck.Show();
+                lblWrong.Hide();
+                picX.Hide();
+                
+            }
+            else
+            {
+                lblWrong.Show();
+                picX.Show();
+                lblRight.Hide();
+                picCheck.Hide();
+
+            }
+
+
+
+            
+
+                
+            
+
         }
     }
 }
