8a18033df2db5996a1dcc0aea24e59019b3d7f49
s487316 <tobi.adebayo@ocsbstudent.ca>
2019-10-25 13:11:09 -0400

    push

--- Rock,Paper,Scissors.Tobi/Rock,Paper,Scissors.Tobi/RockPaperScissorsForm.Designer.cs
+++ Rock,Paper,Scissors.Tobi/Rock,Paper,Scissors.Tobi/RockPaperScissorsForm.Designer.cs
@@ -28,20 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.grbPlayer = new System.Windows.Forms.GroupBox();
+            this.grbComputer = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.grbPlayer.SuspendLayout();
+            this.grbComputer.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // grbPlayer
+            // 
+            this.grbPlayer.Controls.Add(this.radioButton3);
+            this.grbPlayer.Controls.Add(this.radioButton2);
+            this.grbPlayer.Controls.Add(this.radioButton1);
+            this.grbPlayer.Location = new System.Drawing.Point(36, 22);
+            this.grbPlayer.Name = "grbPlayer";
+            this.grbPlayer.Size = new System.Drawing.Size(207, 165);
+            this.grbPlayer.TabIndex = 0;
+            this.grbPlayer.TabStop = false;
+            this.grbPlayer.Text = "Player\'s Choice";
+            // 
+            // grbComputer
+            // 
+            this.grbComputer.Controls.Add(this.radioButton6);
+            this.grbComputer.Controls.Add(this.radioButton5);
+            this.grbComputer.Controls.Add(this.radioButton4);
+            this.grbComputer.Location = new System.Drawing.Point(36, 206);
+            this.grbComputer.Name = "grbComputer";
+            this.grbComputer.Size = new System.Drawing.Size(207, 183);
+            this.grbComputer.TabIndex = 1;
+            this.grbComputer.TabStop = false;
+            this.grbComputer.Text = "Computer\'s Choice";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(29, 48);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(85, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(29, 89);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(85, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(29, 129);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(85, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "radioButton3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(33, 39);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(85, 17);
+            this.radioButton4.TabIndex = 0;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "radioButton4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(33, 85);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(85, 17);
+            this.radioButton5.TabIndex = 1;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "radioButton5";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(33, 133);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(85, 17);
+            this.radioButton6.TabIndex = 2;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "radioButton6";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(36, 395);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(75, 23);
+            this.btnPlay.TabIndex = 2;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
             // 
             // frmRockPaperScissors
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(314, 450);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.grbComputer);
+            this.Controls.Add(this.grbPlayer);
             this.Name = "frmRockPaperScissors";
-            this.Text = "Form1";
+            this.Text = "Rock, Paper, Scissors Tobi";
+            this.Load += new System.EventHandler(this.FrmRockPaperScissors_Load);
+            this.grbPlayer.ResumeLayout(false);
+            this.grbPlayer.PerformLayout();
+            this.grbComputer.ResumeLayout(false);
+            this.grbComputer.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox grbPlayer;
+        private System.Windows.Forms.GroupBox grbComputer;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Button btnPlay;
     }
 }
 
--- Rock,Paper,Scissors.Tobi/Rock,Paper,Scissors.Tobi/RockPaperScissorsForm.cs
+++ Rock,Paper,Scissors.Tobi/Rock,Paper,Scissors.Tobi/RockPaperScissorsForm.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ * Created by: Tobi Adebayo
+ * Created on: 25-10-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #18 - Rock, Paper, Scissors
+ * This program allows the user to play rock paper scissors against the computer
+*/
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +19,22 @@ namespace Rock_Paper_Scissors.Tobi
 {
     public partial class frmRockPaperScissors : Form
     {
+        //Declare local variables and constants
+        const int MIN_VALUE = 1;
+        const int MAX_VALUE = 3;
+        Random randomNumberGenerator;
+
         public frmRockPaperScissors()
         {
             InitializeComponent();
+            //Creat the random number generator object
+            randomNumberGenerator = new Random();
+
+        }
+
+        private void FrmRockPaperScissors_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
