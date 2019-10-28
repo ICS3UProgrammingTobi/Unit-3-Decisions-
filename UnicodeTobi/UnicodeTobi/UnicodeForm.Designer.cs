0facdd52f0977436509e2c118c345c749bd4813d
s487316 <tobi.adebayo@ocsbstudent.ca>
2019-10-27 21:05:17 -0400

    push

---
+++ UnicodeTobi/UnicodeTobi/UnicodeForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace UnicodeTobi
+{
+    partial class frmUnicode
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lstUnicode = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(37, 46);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(170, 75);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lstUnicode
+            // 
+            this.lstUnicode.FormattingEnabled = true;
+            this.lstUnicode.ItemHeight = 16;
+            this.lstUnicode.Location = new System.Drawing.Point(37, 157);
+            this.lstUnicode.Name = "lstUnicode";
+            this.lstUnicode.Size = new System.Drawing.Size(170, 228);
+            this.lstUnicode.TabIndex = 1;
+            // 
+            // frmUnicode
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(279, 450);
+            this.Controls.Add(this.lstUnicode);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmUnicode";
+            this.Text = "Unicode Tobi";
+            this.Load += new System.EventHandler(this.frmUnicode_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lstUnicode;
+    }
+}
+
---
+++ UnicodeTobi/UnicodeTobi/UnicodeForm.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * Created by: Tobi Adebayo
+ * Created on: 27-10-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day 24 - Unicode
+ * This program takes the letters from A to Z along with the Unicode value attached to it and places them in the list box
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UnicodeTobi
+{
+    public partial class frmUnicode : Form
+    {
+        public frmUnicode()
+        {
+            InitializeComponent();
+        }
+
+        private void frmUnicode_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // declare local variables
+            string letters;
+            int letterCounter;
+
+            // Clear the items in listbox
+            this.lstUnicode.Items.Clear();
+
+            // Create loop 
+            for (letterCounter = 65; letterCounter <= 90; letterCounter++)
+            {
+                // convert the counter to unicode letter
+                letters = Char.ConvertFromUtf32(letterCounter);
+
+                // Add letter with its value to the listbox
+                this.lstUnicode.Items.Add(letters + " -> " + letterCounter);
+            }
+
+        }
+    }
+}
