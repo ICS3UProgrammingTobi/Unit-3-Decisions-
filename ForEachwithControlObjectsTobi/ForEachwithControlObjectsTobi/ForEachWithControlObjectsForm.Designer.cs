036e5c1e6dbd3ca4c4643517c00e5e89349725bf
s487316 <tobi.adebayo@ocsbstudent.ca>
2019-10-27 21:27:30 -0400

    push

---
+++ ForEachwithControlObjectsTobi/ForEachwithControlObjectsTobi/ForEachWithControlObjectsForm.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace ForEachwithControlObjectsTobi
+{
+    partial class frmForEachWithControlObjects
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnChangeColour = new System.Windows.Forms.Button();
+            this.lblChangeColour = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnChangeColour
+            // 
+            this.btnChangeColour.Location = new System.Drawing.Point(52, 38);
+            this.btnChangeColour.Name = "btnChangeColour";
+            this.btnChangeColour.Size = new System.Drawing.Size(213, 64);
+            this.btnChangeColour.TabIndex = 0;
+            this.btnChangeColour.Text = "Change Colour";
+            this.btnChangeColour.UseVisualStyleBackColor = true;
+            this.btnChangeColour.Click += new System.EventHandler(this.btnChangeColour_Click);
+            // 
+            // lblChangeColour
+            // 
+            this.lblChangeColour.AutoSize = true;
+            this.lblChangeColour.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblChangeColour.Location = new System.Drawing.Point(46, 207);
+            this.lblChangeColour.Name = "lblChangeColour";
+            this.lblChangeColour.Size = new System.Drawing.Size(219, 32);
+            this.lblChangeColour.TabIndex = 1;
+            this.lblChangeColour.Text = "Change Colour";
+            this.lblChangeColour.Click += new System.EventHandler(this.lblChangeColour_Click);
+            // 
+            // frmForEachWithControlObjects
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(324, 307);
+            this.Controls.Add(this.lblChangeColour);
+            this.Controls.Add(this.btnChangeColour);
+            this.Name = "frmForEachWithControlObjects";
+            this.Text = "For...Each with Control Objects Tobi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnChangeColour;
+        private System.Windows.Forms.Label lblChangeColour;
+    }
+}
+
---
+++ ForEachwithControlObjectsTobi/ForEachwithControlObjectsTobi/ForEachWithControlObjectsForm.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Created by: Tobi adebayo
+ * Created on: 27-10-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #27 - Control Objects
+ * This program changes the color of each object on th form
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ForEachwithControlObjectsTobi
+{
+    public partial class frmForEachWithControlObjects : Form
+    {
+        public frmForEachWithControlObjects()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChangeColour_Click(object sender, EventArgs e)
+        {
+            // loop through each object on the form and make it Yellow
+            foreach (Control aControlObject in this.Controls)
+            {
+                aControlObject.BackColor = Color.Yellow;
+            }
+        }
+
+        private void lblChangeColour_Click(object sender, EventArgs e)
+        {
+            // loop through each object on the form and make it orange
+            foreach (Control aControlObject in this.Controls)
+            {
+                aControlObject.BackColor = Color.Orange;
+
+            }
+        }
+    }
+}
