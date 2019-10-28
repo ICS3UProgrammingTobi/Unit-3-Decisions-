66d5b28b386ec7ac71b47960fc7fbcb222f81c86
s487316 <tobi.adebayo@ocsbstudent.ca>
2019-10-27 21:41:06 -0400

    p

---
+++ NestedLoopsTobi/NestedLoopsTobi/NestedLoopsForm.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace NestedLoopsTobi
+{
+    partial class frmNestedLoops
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lstAlphabets = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(84, 68);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(134, 55);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lstAlphabets
+            // 
+            this.lstAlphabets.FormattingEnabled = true;
+            this.lstAlphabets.ItemHeight = 16;
+            this.lstAlphabets.Location = new System.Drawing.Point(84, 177);
+            this.lstAlphabets.Name = "lstAlphabets";
+            this.lstAlphabets.Size = new System.Drawing.Size(134, 228);
+            this.lstAlphabets.TabIndex = 1;
+            // 
+            // frmNestedLoops
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(312, 450);
+            this.Controls.Add(this.lstAlphabets);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmNestedLoops";
+            this.Text = "Nested Loops Tobi";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lstAlphabets;
+    }
+}
+
---
+++ NestedLoopsTobi/NestedLoopsTobi/NestedLoopsForm.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Created by: Tobi Adebayo
+ * Created on: 27-10-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #28 - Alphabet Nested Loops
+ * This program displays all the letters of the alphabet from A to Z and write all the letters from a to z beside it
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NestedLoopsTobi
+{
+    public partial class frmNestedLoops : Form
+    {
+        public frmNestedLoops()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // declare local variables
+            int capitalCounter;
+            int lowerCounter;
+            string capital;
+            string lowercase;
+
+            // clear listbox
+            this.lstAlphabets.Items.Clear();
+
+            // loop throught the capital letters from 65 to 90
+            for (capitalCounter = 65; capitalCounter <= 90; capitalCounter++)
+            {
+                // For each of the lower alphabets, loop through 97 to 122 and add it beside the capital letters
+                for (lowerCounter = 97; lowerCounter <= 122; lowerCounter++)
+                {
+                    capital = Char.ConvertFromUtf32(capitalCounter);
+                    lowercase = Char.ConvertFromUtf32(lowerCounter);
+
+                    // display in the listbox
+                    this.lstAlphabets.Items.Add(capitalCounter + " -> " + lowerCounter);
+                }
+            }
+        }
+    }
+}
+
