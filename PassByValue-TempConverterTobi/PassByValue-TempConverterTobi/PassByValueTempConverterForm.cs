ff933bb5d9161c00d2906ddd9339c72a1cdb29d1
s487316 <tobi.adebayo@ocsbstudent.ca>
2019-10-30 13:11:09 -0400

    p

--- PassByValue-TempConverterTobi/PassByValue-TempConverterTobi/PassByValueTempConverterForm.cs
+++ PassByValue-TempConverterTobi/PassByValue-TempConverterTobi/PassByValueTempConverterForm.cs
@@ -23,8 +23,14 @@ namespace PassByValue_TempConverterTobi
         {
             InitializeComponent();
         }
+        public void ConvertTofar(double celsius, double farenheit);
 
         private void Frm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
         {
 
         }
