26b117063bfc520781bca1697d653c06b236bb9c
s487316 <tobi.adebayo@ocsbstudent.ca>
2019-10-30 12:36:09 -0400

    p

---
+++ PassByValue-TempConverterTobi/PassByValue-TempConverterTobi/Form1.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace PassByValue_TempConverterTobi
+{
+    partial class frmPassByValueTempConverter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmPassByValueTempConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "frmPassByValueTempConverter";
+            this.Text = "Pass By Value - Temp Converter Tobi";
+            this.Load += new System.EventHandler(this.Frm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ PassByValue-TempConverterTobi/PassByValue-TempConverterTobi/Form1.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by: Tobi Adebayo
+ * Created on: 28-10-30
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #30 - Area Procedure
+ * This program gets the degrees Celsius from the user, passes it by value to a procedure that converts it to Fahrenheit, and then inside the procedure displays the degrees Fahrenheit in a message box
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PassByValue_TempConverterTobi
+{
+    public partial class frmPassByValueTempConverter : Form
+    {
+        public frmPassByValueTempConverter()
+        {
+            InitializeComponent();
+        }
+
+        private void Frm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
